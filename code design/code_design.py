dbd9020a4cf7a0b0b481581feb114461215ff01d
Saeid Haghighatshoar <haghighatshoar@gmail.com>
2022-07-16 00:38:41 +0200

    initial commit.

---
+++ code design/code_design.py
@@ -0,0 +1,13 @@
+# this module investigates uisng the training capabalities of SNN for designing short codes for communications.
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+
+def main():
+	print('end of code design!')
+	pass
+
+
+if __name__ == '__main__':
+	main()
